7cc13dbd1f310c3c4e7ffcf58252d11db5baaa0d
Ing <wjz304@qq.com>
2023-05-26 20:12:31 +0800

    Add /tmpRoot/bin path to sqlite3 from @PeterSuh-Q3

--- reboot/install.sh
+++ reboot/install.sh
@@ -3,7 +3,7 @@
 if [ "${1}" = "late" ]; then
     if [ -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
         echo "insert RebootToArc task"
-        sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "INSERT INFO task VALUES ('RebootToArc', '', 'shutdown', '', 0, 0, 0, 0, '', 0, '/usr/bin/arpl-reboot.sh "config"', 'script', '{}', '', '', '{}', '{}')"
+        /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db "INSERT INFO task VALUES ('RebootToArc', '', 'shutdown', '', 0, 0, 0, 0, '', 0, '/usr/bin/arpl-reboot.sh "config"', 'script', '{}', '', '', '{}', '{}')"
     else
         echo "copy RebootToArc task db"
         mkdir -p /tmpRoot/usr/syno/etc/esynoscheduler
