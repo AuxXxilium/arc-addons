06fc6e04a198e338130dae04572c5471006b4e13
AuxXxilium <info@auxxxilium.tech>
2024-04-09 17:24:31 +0200

    acpid: cleanup
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- acpid/install.sh
+++ acpid/install.sh
@@ -6,9 +6,6 @@ if [ "${1}" = "late" ]; then
   cp -vf "${0}" "/tmpRoot/usr/arc/addons/"
 
   mkdir -p /tmpRoot/etc/acpi/events/
-  # [ ! -f /tmpRoot/etc/acpi/events/power.bak -a -f /tmpRoot/etc/acpi/events/power ] && cp -vf /tmpRoot/etc/acpi/events/power /tmpRoot/etc/acpi/events/power.bak
-  # [ ! -f /tmpRoot/etc/acpi/power.sh.bak -a -f /tmpRoot/etc/acpi/power.sh ] && cp -vf /tmpRoot/etc/acpi/power.sh /tmpRoot/etc/acpi/power.sh.bak
-  # [ ! -f /tmpRoot/usr/sbin/acpid.bak -a -f /tmpRoot/usr/sbin/acpid ] && cp -vf /tmpRoot/usr/sbin/acpid /tmpRoot/usr/sbin/acpid.bak
   cp -vf /etc/acpi/events/power /tmpRoot/etc/acpi/events/power
   cp -vf /etc/acpi/power.sh /tmpRoot/etc/acpi/power.sh
   cp -vf /usr/sbin/acpid /tmpRoot/usr/sbin/acpid
@@ -42,7 +39,4 @@ elif [ "${1}" = "uninstall" ]; then
   rm -f /tmpRoot/etc/acpi/events/power
   rm -f /tmpRoot/etc/acpi/power.sh
   rm -f /tmpRoot/usr/sbin/acpid
-  # [ -f /tmpRoot/etc/acpi/events/power.bak ] && mv -vf /tmpRoot/etc/acpi/events/power.bak /tmpRoot/etc/acpi/events/power
-  # [ -f /tmpRoot/etc/acpi/power.sh.bak ] && mv -vf /tmpRoot/etc/acpi/power.sh.bak /tmpRoot/etc/acpi/power.sh
-  # [ -f /tmpRoot/usr/sbin/acpid.bak ] && mv -vf /tmpRoot/usr/sbin/acpid.bak /tmpRoot/usr/sbin/acpid
 fi
