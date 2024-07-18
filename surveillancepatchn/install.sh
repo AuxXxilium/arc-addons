d5af2c17521f29c2714c3f980506939cad24c361
AuxXxilium <info@auxxxilium.tech>
2024-07-18 19:24:29 +0200

    surveillancepatch: update description
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- surveillancepatchn/install.sh
+++ surveillancepatchn/install.sh
@@ -5,13 +5,13 @@ if [ "${1}" = "late" ]; then
   mkdir -p "/tmpRoot/usr/arc/addons/"
   cp -vf "${0}" "/tmpRoot/usr/arc/addons/"
 
-  cp -vf /usr/bin/surveillancepatchn.sh /tmpRoot/usr/bin/surveillancepatchn.sh
+  cp -vf /usr/bin/surveillancepatch.sh /tmpRoot/usr/bin/surveillancepatch.sh
   cp -vf /usr/lib/libssutils.so /tmpRoot/usr/lib/libssutils.so
   cp -vf /usr/lib/license.sh /tmpRoot/usr/lib/license.sh
   cp -vf /usr/lib/S82surveillance.sh /tmpRoot/usr/lib/S82surveillance.sh
   
   mkdir -p "/tmpRoot/usr/lib/systemd/system"
-  DEST="/tmpRoot/usr/lib/systemd/system/surveillancepatchn.service"
+  DEST="/tmpRoot/usr/lib/systemd/system/surveillancepatch.service"
   echo "[Unit]"                                         >${DEST}
   echo "Description=addon surveillancepatchn"          >>${DEST}
   echo "After=multi-user.target"                       >>${DEST}
@@ -19,13 +19,13 @@ if [ "${1}" = "late" ]; then
   echo "[Service]"                                     >>${DEST}
   echo "Type=oneshot"                                  >>${DEST}
   echo "RemainAfterExit=yes"                           >>${DEST}
-  echo "ExecStart=/usr/bin/surveillancepatchn.sh"      >>${DEST}
+  echo "ExecStart=/usr/bin/surveillancepatch.sh"       >>${DEST}
   echo                                                 >>${DEST}
   echo "[Install]"                                     >>${DEST}
   echo "WantedBy=multi-user.target"                    >>${DEST}
 
   mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
-  ln -vsf /usr/lib/systemd/system/surveillancepatchn.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/surveillancepatchn.service
+  ln -vsf /usr/lib/systemd/system/surveillancepatch.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/surveillancepatch.service
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon surveillancepatchn - ${1}"
   # To-Do
