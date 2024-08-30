b519f2a1ca4e8fac4f1937d0eeaffa117c5971be
AuxXxilium <info@auxxxilium.tech>
2024-08-30 19:06:57 +0200

    videostation: add new
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

---
+++ videostation/install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env ash
+#
+# Copyright (C) 2023 AuxXxilium <https://github.com/AuxXxilium>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+if [ "${1}" = "late" ]; then
+  echo "Installing addon videostation - ${1}"
+  mkdir -p "/tmpRoot/usr/arc/addons/"
+  cp -vf "${0}" "/tmpRoot/usr/arc/addons/"
+  
+  cp -vf /usr/bin/videostation.sh /tmpRoot/usr/bin/videostation.sh
+
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/videostation.service"
+  cat > ${DEST} <<EOF
+[Unit]
+Description=VideoStation for DSM
+After=multi-user.target
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/bin/videostation.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/videostation.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/videostation.service
+elif [ "${1}" = "uninstall" ]; then
+  echo "Installing addon videostation - ${1}"
+
+  rm -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/videostation.service"
+  rm -f "/tmpRoot/usr/lib/systemd/system/videostation.service"
+
+  [ ! -f "/tmpRoot/usr/arc/revert.sh" ] && echo '#!/usr/bin/env bash' >/tmpRoot/usr/arc/revert.sh && chmod +x /tmpRoot/usr/arc/revert.sh
+  echo "/usr/bin/videostation.sh --restore" >> /tmpRoot/usr/arc/revert.sh
+  echo "rm -f /usr/bin/videostation.sh" >> /tmpRoot/usr/arc/revert.sh
+fi
