383ff3c8412bceec2d68efc82bcd4ca11f2ed3f3
AuxXxilium <info@auxxxilium.tech>
2024-09-07 13:53:32 +0200

    arcdns: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- arcdns/all/usr/bin/arcdns.sh
+++ arcdns/all/usr/bin/arcdns.sh
@@ -17,7 +17,7 @@ if [ -f "/etc/ddns_provider.conf" ]; then
     else
         echo "arcdns: Entry ${ENTRY} does not exist, adding now"
         echo "${ENTRY}" >> /etc/ddns_provider.conf
-        echo "        modulepath=/usr/syno/bin/ddns/namecheap.php" >> /etc/ddns_provider.conf
+        echo "        modulepath=/usr/syno/bin/ddns/arcdns.php" >> /etc/ddns_provider.conf
         echo "        queryurl=https://arcdns.tech/update/__HOSTNAME__/__PASSWORD__" >> /etc/ddns_provider.conf
     fi
 fi
@@ -27,7 +27,7 @@ if [ -f "/etc.defaults/ddns_provider.conf" ]; then
     else
         echo "arcdns: Entry ${ENTRY} does not exist, adding now"
         echo "${ENTRY}" >> /etc.defaults/ddns_provider.conf
-        echo "        modulepath=/usr/syno/bin/ddns/namecheap.php" >> /etc.defaults/ddns_provider.conf
+        echo "        modulepath=/usr/syno/bin/ddns/arcdns.php" >> /etc.defaults/ddns_provider.conf
         echo "        queryurl=https://arcdns.tech/update/__HOSTNAME__/__PASSWORD__" >> /etc.defaults/ddns_provider.conf
     fi
 fi
