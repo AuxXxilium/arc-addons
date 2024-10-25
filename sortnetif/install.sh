79ae3e76b0e3fd0d50cad316946aacaaea4f1dc4
AuxXxilium <info@auxxxilium.tech>
2024-10-25 16:04:31 +0200

    sortnetif: rewrite description
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- sortnetif/install.sh
+++ sortnetif/install.sh
@@ -12,19 +12,12 @@ if [ "${1}" = "patches" ]; then
   ETHLIST=""
   ETHX="$(ls /sys/class/net/ 2>/dev/null | grep eth)" # real network cards list
   for ETH in ${ETHX}; do
-    MAC="$(cat /sys/class/net/${ETH}/address 2>/dev/null | sed 's/://g; s/.*/\L&/' | tr '[:lower:]' '[:upper:]')"
+    MAC="$(cat /sys/class/net/${ETH}/address 2>/dev/null | sed 's/://g; s/.*/\L&/')"
     BUS="$(ethtool -i ${ETH} 2>/dev/null | grep bus-info | cut -d' ' -f2)"
     ETHLIST="${ETHLIST}${BUS} ${MAC} ${ETH}\n"
   done
   ETHLISTTMPM=""
   ETHLISTTMPB="$(echo -e "${ETHLIST}" | sort)"
-  if [ -n "${2}" ]; then
-    MACS="$(echo "${2}" | sed 's/://g; s/,/ /g; s/.*/\L&/')"
-    for MACX in ${MACS}; do
-      ETHLISTTMPM="${ETHLISTTMPM}$(echo -e "${ETHLISTTMPB}" | grep "${MACX}")\n"
-      ETHLISTTMPB="$(echo -e "${ETHLISTTMPB}" | grep -v "${MACX}")\n"
-    done
-  fi
   ETHLIST="$(echo -e "${ETHLISTTMPM}${ETHLISTTMPB}" | grep -v '^$')"
   ETHSEQ="$(echo -e "${ETHLIST}" | awk '{print $3}' | sed 's/eth//g')"
   ETHNUM="$(echo -e "${ETHLIST}" | wc -l)"
