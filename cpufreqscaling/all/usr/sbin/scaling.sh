e4725b3b53e4742209d082fca293a62e824430d6
AuxXxilium <info@auxxxilium.tech>
2024-11-20 13:10:39 +0100

    cpufreqscaling: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- cpufreqscaling/all/usr/sbin/scaling.sh
+++ cpufreqscaling/all/usr/sbin/scaling.sh
@@ -12,17 +12,17 @@ set -euo pipefail
 # Load the correct cpufreq module
 cerror=0
 governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-if [ "${1}" == "ondemand" ] || [ "${1}" == "conservative" ]; then
-  insmod /usr/lib/modules/cpufreq_${1}.ko || cerror=1
+if [ "${1}" = "ondemand" ] || [ "${1}" = "conservative" ]; then
+  insmod "/usr/lib/modules/cpufreq_${1}.ko" || cerror=1
 fi
 # Set correct cpufreq governor to allow frequency scaling
 if [ "${governor}" != "${1}" ]; then
-  echo ${1} | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+  echo "${1}" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 fi
 sleep 10
 # Check if the governor is set correctly
 verifygovernor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-if [ "${verifygovernor}" == "${1}" ]; then
+if [ "${verifygovernor}" = "${1}" ]; then
   echo "CPUFreqScaling: Governor set to ${1}"
 else
   echo "CPUFreqScaling: Failed to set governor to ${1}"
