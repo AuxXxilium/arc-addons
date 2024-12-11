1b07183f2ae810b0113cfe8b8f0f6152dcf522ac
AuxXxilium <info@auxxxilium.tech>
2024-12-11 17:37:57 +0100

    tree: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- eudev/install.sh
+++ eudev/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env ash
 #
-# Copyright (C) 2023 AuxXxilium <https://github.com/AuxXxilium> and Ing <https://github.com/wjz304>
+# Copyright (C) 2024 AuxXxilium <https://github.com/AuxXxilium> and Ing <https://github.com/wjz304>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
@@ -48,41 +48,37 @@ elif [ "${1}" = "late" ]; then
   [ ! -L "/tmpRoot/usr/sbin/depmod" ] && ln -vsf /usr/bin/kmod /tmpRoot/usr/sbin/depmod
 
   echo "copy modules"
-  isChange="false"
   export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
+  isChange=false
   /tmpRoot/bin/cp -rnf /usr/lib/firmware/* /tmpRoot/usr/lib/firmware/
-  if cat /proc/version 2>/dev/null | grep -q 'RR@RR'; then
+  if grep -q 'RR@RR' /proc/version 2>/dev/null; then
     if [ -d /tmpRoot/usr/lib/modules.bak ]; then
       /tmpRoot/bin/rm -rf /tmpRoot/usr/lib/modules
       /tmpRoot/bin/cp -rpf /tmpRoot/usr/lib/modules.bak /tmpRoot/usr/lib/modules
     else
-      echo "Custom - backup modules."
+      echo "RR@RR, backup modules."
       /tmpRoot/bin/cp -rpf /tmpRoot/usr/lib/modules /tmpRoot/usr/lib/modules.bak
     fi
     /tmpRoot/bin/cp -rpf /usr/lib/modules/* /tmpRoot/usr/lib/modules
-    echo "true" >/tmp/modulesChange
+    isChange=true
   else
     if [ -d /tmpRoot/usr/lib/modules.bak ]; then
-      echo "Custom - restore modules from backup."
+      echo "RR@RR, restore modules from backup."
       /tmpRoot/bin/rm -rf /tmpRoot/usr/lib/modules
       /tmpRoot/bin/mv -rf /tmpRoot/usr/lib/modules.bak /tmpRoot/usr/lib/modules
     fi
-    cat /addons/modulelist 2>/dev/null | /tmpRoot/bin/sed '/^\s*$/d' | while IFS=' ' read -r O M; do
-      [ "${O:0:1}" = "#" ] && continue
-      [ -z "${M}" -o -z "$(ls /usr/lib/modules/${M} 2>/dev/null)" ] && continue
+    for L in $(/tmpRoot/bin/sed -n '/^\s*$/d; /^\s*#/d; p' /addons/modulelist 2>/dev/null); do
+      O=$(echo "${L}" | awk '{print $1}')
+      M=$(echo "${L}" | awk '{print $2}')
+      [ -z "${M}" ] || [ -z "$(ls /usr/lib/modules/${M} 2>/dev/null)" ] && continue
       if [ "$(echo "${O}" | /tmpRoot/bin/sed 's/.*/\U&/')" = "F" ]; then
-        /tmpRoot/bin/cp -vrf /usr/lib/modules/${M} /tmpRoot/usr/lib/modules/
+      /tmpRoot/bin/cp -vrf /usr/lib/modules/${M} /tmpRoot/usr/lib/modules/
       else
-        /tmpRoot/bin/cp -vrn /usr/lib/modules/${M} /tmpRoot/usr/lib/modules/
+      /tmpRoot/bin/cp -vrn /usr/lib/modules/${M} /tmpRoot/usr/lib/modules/
       fi
-      # isChange="true"
-      # In Bash, the pipe operator | creates a subshell to execute the commands in the pipe.
-      # This means that if you modify a variable inside a while loop,
-      # the modification is not visible outside the loop because the while loop is executed in a subshell.
-      echo "true" >/tmp/modulesChange
+      isChange=true
     done
   fi
-  isChange="$(cat /tmp/modulesChange 2>/dev/null || echo "false")"
   echo "isChange: ${isChange}"
   [ "${isChange}" = "true" ] && /usr/sbin/depmod -a -b /tmpRoot
 
@@ -97,7 +93,7 @@ elif [ "${1}" = "late" ]; then
   DEST="/tmpRoot/usr/lib/systemd/system/udevrules.service"
   {
     echo "[Unit]"
-    echo "Description=Reload udev rules"
+    echo "Description=Arc udev daemon"
     echo
     echo "[Service]"
     echo "Type=oneshot"
@@ -108,6 +104,7 @@ elif [ "${1}" = "late" ]; then
     echo "[Install]"
     echo "WantedBy=multi-user.target"
   } >"${DEST}"
+
   mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
   ln -vsf /usr/lib/systemd/system/udevrules.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/udevrules.service
 fi
