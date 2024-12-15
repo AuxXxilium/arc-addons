345066167b828ac724bed8e443e0d1f98de45540
AuxXxilium <info@auxxxilium.tech>
2024-12-15 20:10:58 +0100

    arccontrol: add new
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

---
+++ arccontrol/all/usr/bin/arccontrol.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2024 AuxXxilium <https://github.com/AuxXxilium>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+APPUPDATE="1.1-16"
+APPVERSION="$(grep -oP '(?<=version=").*(?=")' /var/packages/arc-control/INFO)"
+
+# Function to install Arc Control
+install_arc_control() {
+    if ! synopkg install /usr/arc/addons/arc-control.spk; then
+        echo "Arc Control: Installation failed!"
+        exit 1
+    fi
+}
+
+# Function to uninstall Arc Control
+uninstall_arc_control() {
+    if ! synopkg uninstall arc-control; then
+        echo "Arc Control: Uninstallation failed!"
+        exit 1
+    fi
+}
+
+# Function to set permissions for Arc Control
+set_permissions() {
+    mv /var/packages/arc-control/conf/privilege /tmp
+    mv /var/packages/arc-control/conf/privilege_ /var/packages/arc-control/conf/privilege
+    sed -i 's/package/root/g' /var/packages/arc-control/conf/privilege
+}
+
+# Function to remove task from scheduler
+remove_scheduler_task() {
+    if ! cat /var/packages/arc-control/target/app/tasks.sql | sqlite3 /usr/syno/etc/esynoscheduler/esynoscheduler.db; then
+        echo "Arc Control: Failed to remove task from scheduler!"
+        exit 1
+    fi
+}
+
+# Function to add sudoers for loader disk
+add_sudoers() {
+    if ! echo -e "sc-arc-control ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/99-arc-control /etc.defaults/sudoers.d/99-arc-control > /dev/null; then
+        echo "Arc Control: Failed to add sudoers!"
+        exit 1
+    fi
+    chmod 0440 /etc/sudoers.d/99-arc-control /etc.defaults/sudoers.d/99-arc-control
+}
+
+# Function to start Arc Control
+start_arc_control() {
+    if ! synopkg restart arc-control; then
+        echo "Arc Control: Start failed!"
+        exit 1
+    fi
+}
+
+# Main script execution
+if [ -d "/var/packages/arc-control" ] && [ "${APPUPDATE}" != "${APPVERSION}" ]; then
+    uninstall_arc_control
+    sleep 2
+fi
+
+if [ "${APPUPDATE}" != "${APPVERSION}" ]; then
+    if [ ! -d "/var/packages/python311" ]; then
+        if ! synopkg install /usr/arc/addons/python-3.11.spk; then
+            echo "Python 3.11: Installation failed!"
+            exit 1
+        fi
+    fi
+    sleep 2
+    if [ -d "/var/packages/python311" ]; then
+        if ! synopkg restart python311; then
+            echo "Python 3.11: Start failed!"
+            exit 1
+        fi
+    fi
+    sleep 2
+    if [ ! -d "/var/packages/arc-control" ]; then
+        install_arc_control
+        set_permissions
+        remove_scheduler_task
+        add_sudoers
+    fi
+    sleep 2
+    if [ -d "/var/packages/arc-control" ]; then
+        start_arc_control
+    fi
+fi
---
+++ arccontrol/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env ash
+#
+# Copyright (C) 2024 AuxXxilium <https://github.com/AuxXxilium>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+if [ "${1}" = "late" ]; then
+  echo "Installing addon arccontrol - ${1}"
+  mkdir -p "/tmpRoot/usr/arc/addons/"
+  cp -pf "${0}" "/tmpRoot/usr/arc/addons/"
+
+  cp -pf /usr/bin/arccontrol.sh /tmpRoot/usr/bin/arccontrol.sh
+  cp -pf /usr/arc/addons/arc-control.spk /tmpRoot/usr/arc/addons/arc-control.spk
+  cp -pf /usr/arc/addons/python-3.11.spk /tmpRoot/usr/arc/addons/python-3.11.spk
+
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/arccontrol.service"
+  {
+    echo "[Unit]"
+    echo "Description=addon arccontrol"
+    echo "After=multi-user.target"
+    echo
+    echo "[Service]"
+    echo "User=root"
+    echo "Type=oneshot"
+    echo "RemainAfterExit=yes"
+    echo "ExecStart=/usr/bin/arccontrol.sh"
+    echo
+    echo "[Install]"
+    echo "WantedBy=multi-user.target"
+  } >"${DEST}"
+  mkdir -p /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/arccontrol.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/arccontrol.service
+elif [ "${1}" = "uninstall" ]; then
+  echo "Installing addon arccontrol - ${1}"
+  # To-Do
+fi
