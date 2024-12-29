c8ca83e73b19cc03063034209b1645e23f8f9e08
AuxXxilium <info@auxxxilium.tech>
2024-12-29 15:28:46 +0100

    arccontrol: allow user to run reinstall from inside dsm
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- arccontrol/all/usr/sbin/arccontrol.sh
+++ arccontrol/all/usr/sbin/arccontrol.sh
@@ -19,8 +19,26 @@ install_arc_control() {
 
 # Function to uninstall Arc Control
 uninstall_arc_control() {
+    synopkg stop arc-control
     if ! synopkg uninstall arc-control; then
         echo "Arc Control: Uninstallation failed!"
+        exit 1
+    fi
+}
+
+# Function to install Python 3.11
+install_python3() {
+    if ! synopkg install /usr/arc/addons/python311.spk; then
+        echo "Python 3.11: Installation failed!"
+        exit 1
+    fi
+}
+
+# Function to uninstall Python 3.11
+uninstall_python3() {
+    synopkg stop python311
+    if ! synopkg uninstall python311; then
+        echo "Python 3.11: Uninstallation failed!"
         exit 1
     fi
 }
@@ -33,9 +51,19 @@ set_arc_permissions() {
     fi
 }
 
+# Function to start Python 3.11
+start_python3() {
+    synopkg stop python311
+    if ! synopkg start python311; then
+        echo "Python 3.11: Start failed!"
+        exit 1
+    fi
+}
+
 # Function to start Arc Control
 start_arc_control() {
-    if ! synopkg restart arc-control; then
+    synopkg stop arc-control
+    if ! synopkg start arc-control; then
         echo "Arc Control: Start failed!"
         exit 1
     fi
@@ -43,32 +71,32 @@ start_arc_control() {
 
 # Main script execution
 [ -f "/usr/arc/addons/python-3.11.spk" ] && rm -f "/usr/arc/addons/python-3.11.spk" || true
-if [ -d "/var/packages/arc-control" ] && [ "${APPUPDATE}" != "${APPVERSION}" ]; then
-    uninstall_arc_control
-    sleep 5
+if [ "${APPUPDATE}" != "${APPVERSION}" ]; then
+    if [ -d "/var/packages/arc-control" ]; then
+        uninstall_arc_control
+        sleep 2
+    fi
+    if [ -d "/var/packages/python311" ]; then
+        uninstall_python3
+        sleep 2
+    fi
 fi
 
-if [ ! -d "/var/packages/arc-control" ] || [ "${APPUPDATE}" != "${APPVERSION}" ]; then
+if [ "${APPUPDATE}" != "${APPVERSION}" ]; then
     if [ ! -d "/var/packages/python311" ]; then
-        if ! synopkg install /usr/arc/addons/python311.spk; then
-            echo "Python 3.11: Installation failed!"
-            exit 1
-        fi
+        install_python3
     fi
-    sleep 3
+    sleep 2
     if [ -d "/var/packages/python311" ]; then
-        if ! synopkg restart python311; then
-            echo "Python 3.11: Start failed!"
-            exit 1
-        fi
+        start_python3
     fi
-    sleep 3
+    sleep 2
     if [ ! -d "/var/packages/arc-control" ]; then
         install_arc_control
         set_arc_permissions
     fi
 fi
-sleep 3
+sleep 2
 if [ -d "/var/packages/arc-control" ]; then
     start_arc_control
 fi
