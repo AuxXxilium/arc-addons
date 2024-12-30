16bb2e7db1036d1b2f21129e898bd99b3ff157c9
AuxXxilium <info@auxxxilium.tech>
2024-12-30 17:33:24 +0100

    arccontrol: rework
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- arccontrol/all/usr/sbin/arccontrol.sh
+++ arccontrol/all/usr/sbin/arccontrol.sh
@@ -6,41 +6,25 @@
 # See /LICENSE for more information.
 #
 
-APPUPDATE="1.1-19"
+APPUPDATE="1.1-20"
 APPVERSION="$(grep -oP '(?<=version=").*(?=")' /var/packages/arc-control/INFO | head -n1)"
 
 # Function to install Arc Control
 install_arc_control() {
-    if ! synopkg install /usr/arc/addons/arc-control.spk; then
-        echo "Arc Control: Installation failed!"
-        exit 1
-    fi
+    synopkg install /usr/arc/addons/arc-control.spk
 }
 
 # Function to uninstall Arc Control
 uninstall_arc_control() {
-    synopkg stop arc-control
-    if ! synopkg uninstall arc-control; then
-        echo "Arc Control: Uninstallation failed!"
-        exit 1
+    if synopkg status arc-control | grep -q '"status":"running"'; then
+        synopkg stop arc-control
     fi
+    synopkg uninstall arc-control
 }
 
 # Function to install Python 3.11
 install_python3() {
-    if ! synopkg install /usr/arc/addons/python311.spk; then
-        echo "Python 3.11: Installation failed!"
-        exit 1
-    fi
-}
-
-# Function to uninstall Python 3.11
-uninstall_python3() {
-    synopkg stop python311
-    if ! synopkg uninstall python311; then
-        echo "Python 3.11: Uninstallation failed!"
-        exit 1
-    fi
+    synopkg install /usr/arc/addons/python-3.11.spk
 }
 
 # Function to set permissions for Arc Control
@@ -53,31 +37,28 @@ set_arc_permissions() {
 
 # Function to start Python 3.11
 start_python3() {
-    synopkg stop python311
-    if ! synopkg start python311; then
-        echo "Python 3.11: Start failed!"
-        exit 1
+    if ! synopkg status python3 | grep -q '"status":"running"'; then
+        if ! synopkg start python311; then
+            echo "Python 3.11: Start failed!"
+            exit 1
+        fi
     fi
 }
 
 # Function to start Arc Control
 start_arc_control() {
-    synopkg stop arc-control
-    if ! synopkg start arc-control; then
+    if synopkg status arc-control | grep -q '"status":"running"'; then
+        synopkg restart arc-control
+    elif ! synopkg start arc-control; then
         echo "Arc Control: Start failed!"
         exit 1
     fi
 }
 
 # Main script execution
-[ -f "/usr/arc/addons/python-3.11.spk" ] && rm -f "/usr/arc/addons/python-3.11.spk" || true
 if [ "${APPUPDATE}" != "${APPVERSION}" ]; then
     if [ -d "/var/packages/arc-control" ]; then
         uninstall_arc_control
-        sleep 2
-    fi
-    if [ -d "/var/packages/python311" ]; then
-        uninstall_python3
         sleep 2
     fi
 fi
