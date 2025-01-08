2a7b40358a842539bfbf3240f8124bd8312bc017
AuxXxilium <info@auxxxilium.tech>
2025-01-08 17:05:39 +0100

    cpufreqscaling: rework more dependencies
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- cpufreqscaling/all/usr/sbin/scaling.sh
+++ cpufreqscaling/all/usr/sbin/scaling.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2024 AuxXxilium <https://github.com/AuxXxilium>
+# Copyright (C) 2025 AuxXxilium <https://github.com/AuxXxilium>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
@@ -10,27 +10,29 @@
 set -euo pipefail
 
 # Load the correct cpufreq module
-cerror=0
-if lsmod | grep -q acpi_cpufreq || modprobe acpi_cpufreq; then
+cerror="false"
+if grep -qv "^flags.*hypervisor.*" /proc/cpuinfo; then
   GOVERNOR=$(grep -oP '(?<=governor=)\w+' /proc/cmdline 2>/dev/null)
   SYSGOVERNOR=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-  if [ "${GOVERNOR}" = "ondemand" ] || [ "${GOVERNOR}" = "conservative" ]; then
-    insmod "/usr/lib/modules/cpufreq_${GOVERNOR}.ko" || cerror=1
-  fi
-  # Set correct cpufreq governor to allow frequency scaling
-  if [ "${SYSGOVERNOR}" != "${GOVERNOR}" ]; then
+  if insmod "/usr/lib/modules/cpufreq_${GOVERNOR}.ko"; then
+    # Set correct cpufreq governor to allow frequency scaling
     echo "${GOVERNOR}" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-  fi
-  sleep 10
-  # Check if the governor is set correctly
-  SYSGOVERNOR=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-  if [ "${SYSGOVERNOR}" = "${GOVERNOR}" ]; then
-    echo "CPUFreqScaling: Governor set to ${GOVERNOR}"
+    sleep 5
+    # Check if the governor is set correctly
+    SYSGOVERNOR=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+    if [ "${SYSGOVERNOR}" = "${GOVERNOR}" ]; then
+      echo "CPUFreqScaling: Governor set to ${GOVERNOR}"
+    else
+      echo "CPUFreqScaling: Failed to set governor to ${GOVERNOR}"
+      cerror="true"
+    fi
   else
-    echo "CPUFreqScaling: Failed to set governor to ${GOVERNOR}"
-    cerror=1
+    echo "CPUFreqScaling: Failed to load cpufreq_${GOVERNOR}.ko"
+    cerror="true"
   fi
-else
-  echo "CPUFreqScaling: acpi_cpufreq module could not be loaded"
 fi
-[ ${cerror} -eq 1 ] && exit 1 || exit 0
+if [ "${cerror}" = "true" ]; then
+  exit 1
+else
+  exit 0
+fi
