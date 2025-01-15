66e02fbf0b62abef1ca2262c375dde7e8c518a4d
AuxXxilium <info@auxxxilium.tech>
2025-01-15 19:21:43 +0100

    cpufreqscaling: optimize code
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- cpufreqscaling/all/usr/sbin/scaling.sh
+++ cpufreqscaling/all/usr/sbin/scaling.sh
@@ -15,32 +15,35 @@ SCALINGCOUNT=$(cat /tmp/scaling.count 2>/dev/null || echo 0)
 GOVERNOR="$(grep -o 'governor=[^ ]*' /proc/cmdline 2>/dev/null | cut -d'=' -f2)"
 SYSGOVERNOR="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
 
-if [ "${SYSGOVERNOR}" != "${GOVERNOR}" ]; then
-  if [[ "${GOVERNOR}" = "ondemand" || "${GOVERNOR}" = "conservative" ]]; then
-    /usr/sbin/modprobe "cpufreq_governor" 2>/dev/null
-    if /usr/sbin/modprobe "cpufreq_${GOVERNOR}"; then
-      echo "${GOVERNOR}" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-    else
-      echo "CPUFreqScaling: Failed to load ${GOVERNOR} module" >> /tmp/scaling.log
-    fi
-  elif [[ "${GOVERNOR}" = "schedutil" || "${GOVERNOR}" = "powersave" ]]; then
-    echo "${GOVERNOR}" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-  fi
+if [ ${SCALINGCOUNT} -gt 3 ]; then
+  echo "CPUFreqScaling: Failed to set governor to ${GOVERNOR} after ${SCALINGCOUNT} retries" >> /tmp/scaling.log
+  exit 0
+fi
 
-  sleep 10
+if [ "${SYSGOVERNOR}" != "${GOVERNOR}" ]; then
+  case "${GOVERNOR}" in
+    ondemand|conservative)
+      /usr/sbin/modprobe "cpufreq_governor" 2>/dev/null
+      if /usr/sbin/modprobe "cpufreq_${GOVERNOR}"; then
+        echo "${GOVERNOR}" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+      else
+        echo "CPUFreqScaling: Failed to load ${GOVERNOR} module" >> /tmp/scaling.log
+      fi
+      ;;
+    schedutil|powersave)
+      echo "${GOVERNOR}" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+      ;;
+  esac
+
+  sleep 20
+  echo "CPUFreqScaling: Checking governor" >> /tmp/scaling.log
+
   SYSGOVERNOR="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)"
   if [ "${SYSGOVERNOR}" = "${GOVERNOR}" ]; then
-    echo "CPUFreqScaling: Governor set to ${GOVERNOR}" >> /tmp/scaling.log
+    echo "CPUFreqScaling: governor set to ${GOVERNOR}" >> /tmp/scaling.log
     exit 0
   else
     echo "CPUFreqScaling: Failed to set governor to ${GOVERNOR}" >> /tmp/scaling.log
   fi
 fi
-
-if [ ${SCALINGCOUNT} -gt 3 ]; then
-  echo "CPUFreqScaling: Failed to set governor to ${GOVERNOR} after ${SCALINGCOUNT} retries" >> /tmp/scaling.log
-  exit 0
-else
-  echo $((SCALINGCOUNT + 1)) > /tmp/scaling.count
-  exit 1
-fi
+exit 1
