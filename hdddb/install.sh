d54228f01bd9862f66e1965a1056980cc21c9d4d
AuxXxilium <info@auxxxilium.tech>
2025-01-27 00:39:57 +0100

    tree: fix installs
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- hdddb/install.sh
+++ hdddb/install.sh
@@ -22,7 +22,8 @@ if [ "${1}" = "late" ]; then
   cat <<EOF >"${DEST}"
 [Unit]
 Description=HDDs/SSDs drives databases
-After=multi-user.target
+Wants=smpkg-custom-install.service pkgctl-StorageManager.service
+After=smpkg-custom-install.service
 
 [Service]
 Type=oneshot
