7764ac39e25c7cf3097258fc2e0c42d76487d5c6
AuxXxilium <info@auxxxilium.tech>
2025-01-29 16:21:30 +0100

    tree: bulk rework
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- disks/install.sh
+++ disks/install.sh
@@ -413,11 +413,12 @@ function nondtModel() {
   _set_conf_kv rd "maxdisks" "${MAXDISKS}"
   printf "set maxdisks=%d\n" "${MAXDISKS}"
 
-  if [ "${1}" = "false" ]; then
+  if [ "${1}" = "true" ]; then
     echo "TODO: no-DT's sort!!!"
   fi
 
-  # NVME
+  # NVME + cache handling for models using libsynonvme.so.1
+  [ -f /etc/nvmePorts ] && rm -f /etc/nvmePorts
   COUNT=1
   echo "[pci]" >/etc/extensionPorts
   for P in $(ls -d /sys/block/nvme* 2>/dev/null); do
@@ -425,19 +426,21 @@ function nondtModel() {
       echo "bootloader: ${P}"
       continue
     fi
-    PCIEPATH="$(cat ${P}/uevent 2>/dev/null | grep 'PHYSDEVPATH' | cut -d'=' -f2 | awk -F'/' '{if (NF == 4) print $NF; else if (NF > 4) print $(NF-1)}')"
+    PCIEPATH=$(cat ${P}/uevent 2>/dev/null | grep 'PHYSDEVPATH' | rev | cut -d'/' -f2 | rev )
     if [ -n "${PCIEPATH}" ]; then
       grep -q "=\"${PCIEPATH}\"" /etc/extensionPorts && continue # An nvme controller only recognizes one disk
       echo "pci${COUNT}=\"${PCIEPATH}\"" >>/etc/extensionPorts
+      echo "${PCIEPATH}" >>/etc/nvmePorts
       COUNT=$((${COUNT} + 1))
 
       _set_conf_kv rd "supportnvme" "yes"
       _set_conf_kv rd "support_m2_pool" "yes"
     fi
   done
+  [ -f /etc/nvmePorts ] && cat /etc/nvmePorts
 }
 
-#
+### Main
 if [ "${1}" = "patches" ]; then
   echo "Installing addon disks - ${1}"
 
@@ -457,6 +460,57 @@ if [ "${1}" = "patches" ]; then
 
 elif [ "${1}" = "late" ]; then
   echo "Installing addon disks - ${1}"
+
+  MODELS="DS918+ RS1619xs+ DS419+ DS1019+ DS719+ DS1621xs+"
+  MODEL=$(cat /proc/sys/kernel/syno_hw_version)
+
+  # NVMe cache handling for models using libsynonvme.so.1
+  if echo ${MODELS} | grep -q ${MODEL}; then
+  #
+  # |       models      |     1st      |     2nd      |
+  # | DS918+            | 0000:00:13.1 | 0000:00:13.2 |
+  # | RS1619xs+         | 0000:00:03.2 | 0000:00:03.3 |
+  # | DS419+, DS1019+   | 0000:00:14.1 |              |
+  # | DS719+, DS1621xs+ | 0000:00:01.1 | 0000:00:01.0 |
+  #
+  # In the late stage, the /sys/ directory does not exist, and the device path cannot be obtained.
+  # (/dev/ does exist, but there is no useful information.)
+  # (The information obtained by lspci is incomplete and an error will be reported.)
+  # Therefore, the device path is obtained in the early stage and stored in /etc/nvmePorts.
+
+    SO_FILE="/tmpRoot/usr/lib/libsynonvme.so.1"
+    [ ! -f "${SO_FILE}.bak" ] && cp -vf "${SO_FILE}" "${SO_FILE}.bak"
+
+    cp -vf "${SO_FILE}.bak" "${SO_FILE}"
+
+    num=1
+    while read -r N; do
+      echo "${num} - ${N}"
+      if [ ${num} -eq 1 ]; then
+        if [ ${MODEL} = "DS918+" ]; then 
+          sed -i "s/0000:00:13.1/${N}/" "${SO_FILE}"
+        elif [ ${MODEL} = "RS1619xs+" ]; then
+          sed -i "s/0000:00:03.2/${N}/" "${SO_FILE}"
+        elif [ ${MODEL} = "DS419+" ] || [ ${MODEL} = "DS1019+" ]; then
+          sed -i "s/0000:00:14.1/${N}/" "${SO_FILE}"
+        elif [ ${MODEL} = "DS719+" ] || [ ${MODEL} = "DS1621xs+" ]; then
+          sed -i "s/0000:00:01.1/${N}/" "${SO_FILE}"
+        fi  
+      elif [ ${num} -eq 2 ]; then
+        if [ ${MODEL} = "DS918+" ]; then 
+          sed -i "s/0000:00:13.2/${N}/" "${SO_FILE}"
+        elif [ ${MODEL} = "RS1619xs+" ]; then
+          sed -i "s/0000:00:03.3/${N}/" "${SO_FILE}"        
+        elif [ ${MODEL} = "DS719+" ] || [ ${MODEL} = "DS1621xs+" ]; then
+          sed -i "s/0000:00:01.0/${N}/" "${SO_FILE}"        
+        fi  
+      else
+        break
+      fi
+      num=$((num + 1))
+    done < /etc/nvmePorts
+  fi
+  
   if [ "$(_get_conf_kv rd supportportmappingv2)" = "yes" ]; then
     echo "Copying /etc.defaults/model.dtb"
     # copy file
