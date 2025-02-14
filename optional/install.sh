67021edbfeb424872061264f1b019b84fada134b
AuxXxilium <info@auxxxilium.tech>
2025-02-14 21:19:10 +0100

    tree: fix some more
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- optional/install.sh
+++ optional/install.sh
@@ -11,12 +11,21 @@ install_addon() {
   mkdir -p "/tmpRoot/usr/arc/addons/"
   cp -pf "${0}" "/tmpRoot/usr/arc/addons/"
 
-  # logger
+  # libsynosata.so.1
   SO_FILE="/tmpRoot/usr/lib/libsynosata.so.1"
   [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
   cp -pf "${SO_FILE}" "${SO_FILE}.tmp"
   xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
     sed "s/8d15ba160000bf03000000e8c0c8ffff/8d15ba160000bf030000009090909090/" |
+    xxd -r -p >"${SO_FILE}" 2>/dev/null
+  rm -f "${SO_FILE}.tmp"
+
+  # libhwcontrol.so.1
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  [ ! -f "${SO_FILE}.bak" ] && cp -pf "${SO_FILE}" "${SO_FILE}.bak"
+  cp -pf "${SO_FILE}" "${SO_FILE}.tmp"
+  xxd -c $(xxd -p "${SO_FILE}.tmp" 2>/dev/null | wc -c) -p "${SO_FILE}.tmp" 2>/dev/null |
+    sed "s/8d1512840900bf03000000e8d0fcfdff/8D1512840900BF030000009090909090/; s/8d159a810900bf03000000e858fafdff/8D159A810900BF030000009090909090/" |
     xxd -r -p >"${SO_FILE}" 2>/dev/null
   rm -f "${SO_FILE}.tmp"
 
@@ -40,7 +49,10 @@ uninstall_addon() {
   echo "Uninstalling addon optional - ${1}"
 
   SO_FILE="/tmpRoot/usr/lib/libsynosata.so.1"
-  [ -f "${SO_FILE}.bak" ] && mv -pf "${SO_FILE}.bak" "${SO_FILE}"
+  [ -f "${SO_FILE}.bak" ] && mv -f "${SO_FILE}.bak" "${SO_FILE}"
+
+  SO_FILE="/tmpRoot/usr/lib/libhwcontrol.so.1"
+  [ -f "${SO_FILE}.bak" ] && mv -f "${SO_FILE}.bak" "${SO_FILE}"
 
   # Restore syslog-ng configurations
   if [ -f /tmpRoot/etc.defaults/syslog-ng/patterndb.d/scemd.conf.bak ]; then
