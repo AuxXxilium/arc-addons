8d159cfc847a07919cee64a4db6d10725266a625
AuxXxilium <info@auxxxilium.tech>
2025-04-27 17:59:15 +0200

    redpill: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- redpill/install.sh
+++ redpill/install.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env sh
 #
-# Copyright (C) 2025 AuxXxilium <https://github.com/AuxXxilium> and Ing <https://github.com/wjz304>
+# Copyright (C) 2025 AuxXxilium <https://github.com/AuxXxilium>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
 
 install_addon() {
+  SUPPORTED="false"
   PLATFORMS="epyc7002 v1000nk"
   PLATFORM="$(/bin/get_key_value /etc.defaults/synoinfo.conf unique | cut -d"_" -f2)"
-  if ! echo "${PLATFORMS}" | grep -qw "${PLATFORM}"; then
-    echo "${PLATFORM} is not supported redpill addon!"
-    exit 0
+
+  # Check if the platform is supported
+  for SUPPORTED_PLATFORM in ${PLATFORMS}; do
+    if [ "${PLATFORM}" = "${SUPPORTED_PLATFORM}" ]; then
+      SUPPORTED="true"
+      break
+    fi
+  done
+
+  if [ "${SUPPORTED}" != "true" ]; then
+    echo "${PLATFORM} is not supported for the redpill addon!"
+    exit 1
   fi
 
+  # Handle installation based on the argument
   case "${1}" in
     early)
       echo "Installing addon redpill - early"
@@ -21,7 +32,7 @@ install_addon() {
       ;;
     jrExit)
       echo "Installing addon redpill - jrExit"
-      #rmmod redpill
+      rmmod redpill
       ;;
     *)
       exit 0
