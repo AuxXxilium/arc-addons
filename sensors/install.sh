abb6c04fca875c86344cc774a3216f02e772cb15
AuxXxilium <info@auxxxilium.tech>
2025-06-07 18:55:04 +0200

    fancontrol/sensors: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- sensors/install.sh
+++ sensors/install.sh
@@ -14,19 +14,18 @@ if [ "${1}" = "late" ]; then
   tar -zxf /addons/sensors-7.1.tgz -C /tmpRoot/usr/
   cp -vpf /usr/bin/arc-sensors.sh /tmpRoot/usr/bin/arc-sensors.sh
 
-  if [ -f "/addons/fancontrol.sh" ]; then
-    export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
-    ESYNOSCHEDULER_DB="/tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db"
-    if [ ! -f "${ESYNOSCHEDULER_DB}" ] || ! /tmpRoot/usr/bin/sqlite3 "${ESYNOSCHEDULER_DB}" ".tables" | grep -wq "task"; then
-      echo "copy esynoscheduler.db"
-      mkdir -p "$(dirname "${ESYNOSCHEDULER_DB}")"
-      cp -vpf /addons/esynoscheduler.db "${ESYNOSCHEDULER_DB}"
-    fi
-    if echo "SELECT * FROM task;" | /tmpRoot/usr/bin/sqlite3 "${ESYNOSCHEDULER_DB}" | grep -E "Fancontrol" -A10 | grep -Eq "^FANMODES=(.*)$"; then
-      echo "Fancontrol task already exists"
-    else
-        echo "insert sensors task to esynoscheduler.db"
-      /tmpRoot/usr/bin/sqlite3 "${ESYNOSCHEDULER_DB}" <<EOF
+  export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
+  ESYNOSCHEDULER_DB="/tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db"
+  if [ ! -f "${ESYNOSCHEDULER_DB}" ] || ! /tmpRoot/usr/bin/sqlite3 "${ESYNOSCHEDULER_DB}" ".tables" | grep -wq "task"; then
+    echo "copy esynoscheduler.db"
+    mkdir -p "$(dirname "${ESYNOSCHEDULER_DB}")"
+    cp -vpf /addons/esynoscheduler.db "${ESYNOSCHEDULER_DB}"
+  fi
+  if echo "SELECT * FROM task;" | /tmpRoot/usr/bin/sqlite3 "${ESYNOSCHEDULER_DB}" | grep -E "Fancontrol" -A10 | grep -Eq "^FANMODES=(.*)$"; then
+    echo "Fancontrol task already exists"
+  else
+    echo "insert sensors task to esynoscheduler.db"
+    /tmpRoot/usr/bin/sqlite3 "${ESYNOSCHEDULER_DB}" <<EOF
 DELETE FROM task WHERE task_name LIKE 'Fancontrol';
 INSERT INTO task VALUES('Fancontrol', '', 'bootup', '', 0, 0, 0, 0, '', 0, '
 # You only need to modify the following 12 values. You need to change the FanMode once. You do not need to run the task.
@@ -40,44 +39,27 @@ FANMODES=("20 50 100 50" "20 60 80 20" "20 60 50 10")
 # MINSTART and MINSTOP are in percent (0-100)
 ', 'script', '{}', '', '', '{}', '{}');
 EOF
-    fi
-
-    mkdir -p "/tmpRoot/usr/lib/systemd/system"
-    DEST="/tmpRoot/usr/lib/systemd/system/sensors.service"
-    {
-      echo "[Unit]"
-      echo "Description=sensors daemon"
-      echo "After=multi-user.target"
-      echo
-      echo "[Service]"
-      echo "Type=forking"
-      echo "ExecStart=/usr/bin/arc-sensors.sh"
-      echo "ExecReload=/usr/bin/pkill -f /usr/bin/arc-sensors.sh"
-      echo "Restart=always"
-      echo
-      echo "[Install]"
-      echo "WantedBy=multi-user.target"
-    } >"${DEST}"
-
-    mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
-    ln -vsf /usr/lib/systemd/system/sensors.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/sensors.service
-  else
-    echo "fancontrol disabled, skipping installation"
-    export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
-    ESYNOSCHEDULER_DB="/tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db"
-    if [ -f "${ESYNOSCHEDULER_DB}" ]; then
-      echo "delete fancontrol task from esynoscheduler.db"
-      /tmpRoot/usr/bin/sqlite3 "${ESYNOSCHEDULER_DB}" <<EOF
-DELETE FROM task WHERE task_name LIKE 'Fancontrol';
-EOF
-    fi
-    rm -f /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/sensors.service
-    rm -f /tmpRoot/usr/lib/systemd/system/sensors.service
   fi
 
-  [ -f "/tmpRoot/usr/bin/arc-fancontrol.sh" ] && rm -f /tmpRoot/usr/bin/arc-fancontrol.sh
-  [ -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/fancontrol.service" ] && rm -f "/tmpRoot/usr/lib/systemd/system/multi-user.target.wants/fancontrol.service"
-  [ -f "/tmpRoot/usr/lib/systemd/system/fancontrol.service" ] && rm -f "/tmpRoot/usr/lib/systemd/system/fancontrol.service"
+  mkdir -p "/tmpRoot/usr/lib/systemd/system"
+  DEST="/tmpRoot/usr/lib/systemd/system/sensors.service"
+  {
+    echo "[Unit]"
+    echo "Description=sensors daemon"
+    echo "After=multi-user.target"
+    echo
+    echo "[Service]"
+    echo "Type=forking"
+    echo "ExecStart=/usr/bin/arc-sensors.sh"
+    echo "ExecReload=/usr/bin/pkill -f /usr/bin/arc-sensors.sh"
+    echo "Restart=always"
+    echo
+    echo "[Install]"
+    echo "WantedBy=multi-user.target"
+  } >"${DEST}"
+
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/sensors.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/sensors.service
 
 elif [ "${1}" = "uninstall" ]; then
   echo "Installing addon sensors - ${1}"
