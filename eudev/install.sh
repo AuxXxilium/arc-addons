71b4e36e097c3dc0148bd5256a21f30f8419deb2
AuxXxilium <info@auxxxilium.tech>
2025-06-11 21:33:47 +0200

    eudev: test
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- eudev/install.sh
+++ eudev/install.sh
@@ -25,7 +25,7 @@ install_modules() {
   echo "Triggering add events to udev"
   udevadm trigger --type=subsystems --action=add
   udevadm trigger --type=devices --action=add
-  udevadm trigger --type=devices --action=change
+  # udevadm trigger --type=devices --action=change
   udevadm settle --timeout=30 || echo "udevadm settle failed"
   sleep 10
   # Remove from memory to not conflict with RAID mount scripts
