fb698ffd85c8e26b7b4ba1de4c0ca42570c0532c
AuxXxilium <info@auxxxilium.tech>
2025-06-12 18:04:16 +0200

    tree: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- nvmesystem/install.sh
+++ nvmesystem/install.sh
@@ -60,7 +60,7 @@ elif [ "${1}" = "late" ]; then
     echo "Description=nvmesystem daemon"
     echo "Wants=smpkg-custom-install.service pkgctl-StorageManager.service"
     echo "After=smpkg-custom-install.service"
-    echo "After=storagepanel.service" # storagepanel
+    echo "After=storagepanel.service"
     echo
     echo "[Service]"
     echo "Type=oneshot"
