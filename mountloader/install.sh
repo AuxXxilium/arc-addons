f2723f0e99754677e2959e951bcf7186be010f4a
AuxXxilium <info@auxxxilium.tech>
2025-09-27 18:55:23 +0200

    mountloader: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- mountloader/install.sh
+++ mountloader/install.sh
@@ -19,22 +19,9 @@ if [ "${1}" = "late" ]; then
   cp -vpf /usr/bin/yq /tmpRoot/usr/bin/yq
   cp -vpf /usr/bin/unzip /tmpRoot/usr/bin/unzip
 
-  if [ -f /usr/bin/arcsu ]; then
-    cp -vpf /usr/bin/arcsu /tmpRoot/usr/bin/arcsu
-    chown root:root /tmpRoot/usr/bin/arcsu
-    chmod u+s /tmpRoot/usr/bin/arcsu
-  fi
-
-  [ ! -f /tmpRoot/sbin/fatlabel ] && cp -vpf /usr/sbin/fatlabel /tmpRoot/sbin/fatlabel
-  [ ! -f /tmpRoot/sbin/dosfslabel ] && ln -vsf fatlabel /tmpRoot/sbin/dosfslabel
-  [ ! -f /tmpRoot/sbin/fsck.fat ] && cp -vpf /usr/sbin/fsck.fat /tmpRoot/sbin/fsck.fat
-  [ ! -f /tmpRoot/sbin/dosfsck ] && ln -vsf fsck.fat /tmpRoot/sbin/dosfsck
-  [ ! -f /tmpRoot/sbin/fsck.msdos ] && ln -vsf fsck.fat /tmpRoot/sbin/fsck.msdos
-  [ ! -f /tmpRoot/sbin/fsck.vfat ] && ln -vsf fsck.fat /tmpRoot/sbin/fsck.vfat
-  [ ! -f /tmpRoot/sbin/mkfs.fat ] && cp -vpf /usr/sbin/mkfs.fat /tmpRoot/sbin/mkfs.fat
-  [ ! -f /tmpRoot/sbin/mkdosfs ] && ln -vsf mkfs.fat /tmpRoot/sbin/mkdosfs
-  [ ! -f /tmpRoot/sbin/mkfs.msdos ] && ln -vsf mkfs.fat /tmpRoot/sbin/mkfs.msdos
-  [ ! -f /tmpRoot/sbin/mkfs.vfat ] && ln -vsf mkfs.fat /tmpRoot/sbin/mkfs.vfat
+  cp -vpf /usr/bin/arcsu /tmpRoot/usr/bin/arcsu
+  chown root:root /tmpRoot/usr/bin/arcsu
+  chmod 4755 /tmpRoot/usr/bin/arcsu
 
   rm -f /tmpRoot/usr/arc/.mountloader
 elif [ "${1}" = "uninstall" ]; then
