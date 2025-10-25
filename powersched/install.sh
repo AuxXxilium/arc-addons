3f0e973797432dd2d5177bfc2c25a71de666566a
AuxXxilium <info@auxxxilium.tech>
2025-10-25 22:07:39 +0200

    powersched: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- powersched/install.sh
+++ powersched/install.sh
@@ -11,19 +11,16 @@ if [ "${1}" = "late" ]; then
   mkdir -p "/tmpRoot/usr/arc/addons/"
   cp -pf "${0}" "/tmpRoot/usr/arc/addons/"
 
-  [ ! -f "/tmpRoot/usr/sbin/powersched.bak" -a -f "/tmpRoot/usr/sbin/powersched" ] && cp -pf "/tmpRoot/usr/sbin/powersched" "/tmpRoot/usr/sbin/powersched.bak"
-  cp -pf "/usr/sbin/powersched" "/tmpRoot/usr/sbin/powersched"
-  chmod 755 "/tmpRoot/usr/sbin/powersched"
+  cp -vpf /usr/sbin/powersched /tmpRoot/usr/sbin/powersched
 
-  # Clean old entries
-  [ ! -f "/tmpRoot/etc/crontab.bak" -a -f "/tmpRoot/etc/crontab" ] && cp -f "/tmpRoot/etc/crontab" "/tmpRoot/etc/crontab.bak"
-  sed -i '/\/usr\/sbin\/powersched/d' /tmpRoot/etc/crontab
+  [ ! -f "/tmpRoot/etc/crontab.bak" ] && [ -f "/tmpRoot/etc/crontab" ] && cp -pf "/tmpRoot/etc/crontab" "/tmpRoot/etc/crontab.bak"
+  sed -i '/\/usr\/sbin\/powersched/d' /tmpRoot/etc/crontab 2>/dev/null
+  echo "*       *       *       *       *       root    /usr/sbin/powersched" >>/tmpRoot/etc/crontab
 
-  # Add line to crontab, execute each minute
-  echo "*       *       *       *       *       root    /usr/sbin/powersched #arpl powersched addon" >>/tmpRoot/etc/crontab
 elif [ "${1}" = "uninstall" ]; then
-  echo "Uninstalling addon powersched - ${1}"
+  echo "Installing addon powersched - ${1}"
 
-  [ -f "/tmpRoot/usr/sbin/powersched.bak" ] && mv -f "/tmpRoot/usr/sbin/powersched.bak" "/tmpRoot/usr/sbin/powersched"
+  rm -f /tmpRoot/usr/sbin/powersched
   [ -f "/tmpRoot/etc/crontab.bak" ] && mv -f "/tmpRoot/etc/crontab.bak" "/tmpRoot/etc/crontab"
+  sed -i '/\/usr\/sbin\/powersched/d' /tmpRoot/etc/crontab 2>/dev/null
 fi
