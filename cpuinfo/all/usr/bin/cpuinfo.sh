01f2ce382c0d2091344724fecb70c3a055caf917
AuxXxilium <info@auxxxilium.tech>
2026-02-07 17:08:51 +0100

    cpuinfo: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- cpuinfo/all/usr/bin/cpuinfo.sh
+++ cpuinfo/all/usr/bin/cpuinfo.sh
@@ -9,8 +9,7 @@
 VENDOR=""                                                                               # str
 FAMILY=""                                                                               # str
 SERIES="$(grep -m1 'model name' /proc/cpuinfo 2>/dev/null | cut -d: -f2 | sed -E 's/@ [0-9.]+[[:space:]]*GHz//g' | sed -E 's/ CPU//g' | xargs)"       # str
-CORES="$(grep -c 'cpu cores' /proc/cpuinfo 2>/dev/null)"                                # str
-SPEED="$(grep -m1 'MHz' /proc/cpuinfo 2>/dev/null | cut -d: -f2 | cut -d. -f1 | xargs)" # int
+CORES="$(cat /sys/devices/system/cpu/cpu[0-9]*/topology/{core_cpus_list,thread_siblings_list} | sort -u | wc -l 2>/dev/null)"                                # str
 GOVERNOR="$(cat /proc/cmdline 2>/dev/null | grep -oE 'governor=[^ ]+' | cut -d= -f2 | xargs)" # str
 MEV="$(cat "/proc/cmdline" 2>/dev/null | grep -oE 'mev=[^ ]+' | cut -d= -f2 | xargs)" # str
 if [ -n "${MEV}" ] && [ "${MEV}" != "physical" ]; then
