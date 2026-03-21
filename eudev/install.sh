e522c741c32fa3d23bc74408ee45bec5fe38f672
AuxXxilium <info@auxxxilium.tech>
2026-03-21 16:05:59 +0100

    eudev: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- eudev/install.sh
+++ eudev/install.sh
@@ -44,8 +44,9 @@ elif [ "${1}" = "modules" ]; then
     exit 1
   }
   echo "Triggering events to udev"
-  udevadm trigger --action=add
-  udevadm trigger --action=change
+  udevadm trigger --type=subsystems --action=add
+  udevadm trigger --type=devices --action=add
+  udevadm trigger --type=devices --action=change
   udevadm settle --timeout=30 || echo "udevadm settle failed"
   # Give more time
   sleep 10
